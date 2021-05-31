5b61175f11a148ecb183c0b7c377b07c2ee68e85
johnny <327983969@qq.com>
2021-05-31 13:32:08 +0800

    .

--- DoubanMovie250/flaskDouban/app.py
+++ DoubanMovie250/flaskDouban/app.py
@@ -1,17 +1,43 @@
 from flask import Flask,render_template,flash,request
-import datetime
+import sqlite3
+
+
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
+def index():
     return render_template('index.html')
 
-@app.route('/result',methods=['POST','GET'])
-def result():
-    if request.method=='POST':
-        result = request.form
-        return render_template('result.html',result = result)
+
+@app.route('/index')
+def hello_world():
+    return index()
+
+@app.route('/movie')
+def movie():
+    dataList = []
+    conn = sqlite3.connect('movie250.db')
+    cur = conn.cursor()
+    sql = "select * from movie250"
+    data = cur.execute(sql)
+    for item in data:
+        dataList.append(item)
+    cur.close()
+    conn.close()
+    return render_template('movie.html',movies = dataList)
+
+@app.route('/score')
+def score():
+    return render_template('score.html')
+
+@app.route('/word')
+def word():
+    return render_template('word.html')
+
+@app.route('/team')
+def team():
+    return render_template('team.html')
 
 
 if __name__ == '__main__':
