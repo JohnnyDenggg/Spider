8ba0de3c89202769000594f9cec7adedbc5cd66d
johnny <327983969@qq.com>
2021-05-31 16:56:08 +0800

    .

--- DoubanMovie250/flaskDouban/app.py
+++ DoubanMovie250/flaskDouban/app.py
@@ -1,5 +1,10 @@
 from flask import Flask,render_template,flash,request
 import sqlite3
+import jieba
+from matplotlib import pyplot as plt
+from wordcloud import WordCloud
+from PIL import Image
+import numpy as np
 
 
 app = Flask(__name__)
@@ -29,10 +34,33 @@ def movie():
 
 @app.route('/score')
 def score():
-    return render_template('score.html')
+    score =[]
+    num = []
+    conn = sqlite3.connect('movie250.db')
+    cur = conn.cursor()
+    sql = "select score,count(score) from movie250 group by score"
+    data = cur.execute(sql)
+    for item in data:
+        score.append(item[0])
+        num.append(item[1])
+    cur.close()
+    conn.close()
+    return render_template('score.html',score=score,num=num)
 
 @app.route('/word')
 def word():
+    conn = sqlite3.connect('movie250.db')
+    cur = conn.cursor()
+    sql = "select introduction from movie250"
+    data = cur.execute(sql)
+    text = ""
+    for item in data:
+        text = text + item[0]
+    print(text)  # 词云所需的数据
+    cut = jieba.cut(text)
+
+    cur.close()
+    conn.close()
     return render_template('word.html')
 
 @app.route('/team')
